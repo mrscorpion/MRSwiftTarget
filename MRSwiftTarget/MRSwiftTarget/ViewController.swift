3a49690854fcef83578b967aef25cf21966085b7
ms <1515679729@qq.com>
2016-12-26 20:17:21 +0800

    add

---
+++ MRSwiftTarget/MRSwiftTarget/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MRSwiftTarget
+//
+//  Created by ms on 2016/12/26.
+//  Copyright © 2016年 mrscorpion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
